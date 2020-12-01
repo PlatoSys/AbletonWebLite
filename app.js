346635fe84df5b2308051096850e36d5118c457b
PlatoSys <platoniinjgia@gmail.com>
2020-12-02 01:00:19 +0400

    Recording Function

--- app.js
+++ app.js
@@ -84,40 +84,75 @@
 // ==================
 
 
+
+
+window.onresize = reportWindowSize;
+
+let recordedNotes = new Object();
+recordedNotes = {
+
+}
+
+
 let keyboardCanPlay = document.querySelector('#OnOff').getAttribute('playable');
 let metronome = document.querySelector('#metronomeButton').getAttribute('metronome');
 let timer;
-let movingLine;
+let noteLength = parseInt(document.querySelector('.note').offsetWidth);
 let verticalLine1 = document.createElement('div');
 verticalLine1.classList.add('verticalLine');
 let verticalLine2 = document.createElement('div');
 verticalLine2.classList.add('verticalLine');
 let verticalLine3 = document.createElement('div');
 verticalLine3.classList.add('verticalLine');
+let playerPanelWidth = document.querySelector('.player-panel').offsetWidth;
+let movingLine = document.createElement('div');
+movingLine.classList.add('movingLine');
+movingLine.style.left = `${playerPanelWidth + noteLength}px`;
+let notes = document.querySelector('.notes');
+notes.appendChild(movingLine);
 
 
-(function () { 
-    let notes = document.querySelector('.notes');
+function reportWindowSize() {
+
     let noteLength = parseInt(document.querySelector('.note').offsetWidth);
     let lineWidth = ((parseInt(document.querySelector('.notesfordiv').offsetWidth))/4);
-    let metronomeLine = (parseInt(document.querySelector('.line').offsetWidth)-noteLength)/12;
+    let quarters = document.querySelectorAll('.metronomeLine');
+    let metronomeLine = (parseInt(document.querySelector('.line').offsetWidth)-noteLength)/16;
+    movingLine.style.left = `${playerPanelWidth + noteLength}px`;
+    verticalLine1.style.left = `${playerPanelWidth + noteLength + lineWidth}px`;
+    verticalLine2.style.left = `${playerPanelWidth + noteLength + 2*lineWidth}px`;
+    verticalLine3.style.left = `${playerPanelWidth + noteLength + (3*lineWidth)}px`;
+    let i = 1;
+    quarters.forEach(element => {   
+        element.style.left = `${playerPanelWidth + noteLength + (metronomeLine) * i}px`;
+        i ++;
+    });
+}
+
+  
+(function () { 
+    let noteLength = parseInt(document.querySelector('.note').offsetWidth);
+    let lineWidth = ((parseInt(document.querySelector('.notesfordiv').offsetWidth))/4);
+    let metronomeLine = (parseInt(document.querySelector('.line').offsetWidth)-noteLength)/16;
     let movLine = document.querySelector('.moveLine');
 
-    console.log(lineWidth);
-    verticalLine1.style.left = `${noteLength + lineWidth}px`;
-    verticalLine2.style.left = `${(2*lineWidth)}px`;
-    verticalLine3.style.left = `${noteLength + (3*lineWidth)}px`;
+    verticalLine1.style.left = `${playerPanelWidth + noteLength + lineWidth}px`;
+    verticalLine2.style.left = `${playerPanelWidth + noteLength + 2*lineWidth}px`;
+    verticalLine3.style.left = `${playerPanelWidth + noteLength + (3*lineWidth)}px`;
     movLine.appendChild(verticalLine1);
     movLine.appendChild(verticalLine2);
     movLine.appendChild(verticalLine3);
 
-    for(let i = 1; i <= 12; i++){
-        // console.log(metronomeLine * i)
+    for(let i = 1; i <= 16; i++){
         let line = document.createElement('div');
         line.classList.add('metronomeLine');
-        line.style.left = `${metronomeLine * i}px`;
-        // notes.appendChild(line);
+        if(i == 16){
+            line.classList.add('lastmetronomeLine')
+        }
+        line.style.left = `${playerPanelWidth + noteLength + (metronomeLine) * i}px`;
+        notes.appendChild(line);
     }
+
 })();
 
 
@@ -128,20 +163,52 @@ function start(){
     if(metronome == 'true'){
         let audio = new Audio(`/Notes/metronomesound.mp3`);
         audio.play();
-        console.log(bpm)
         timer = setInterval(metronomeSound, bpm);
-        movingLine = setInterval(moveLine, bpm);
+
+        let LineDoMove = document.querySelector('.movingLine');
+        let noteLength = parseInt(document.querySelector('.note').offsetWidth);
+        let lineWidth = document.querySelector('.line');
+        let firstMetronomeLine = document.querySelector('.metronomeLine');
+        let Speed = (parseInt(firstMetronomeLine.offsetLeft) - playerPanelWidth - noteLength)/50;
+    
+        previousPos = parseInt(document.querySelector('.movingLine').offsetLeft);
+        LineDoMove.style.left = `${previousPos + Speed}px`;
+        movingLine = setInterval(moveLine, bpm / 50 );
     }
+}
+
+function pause(){
+    clearInterval(timer);
+    clearInterval(movingLine);
+    console.log('paused');
 }
 
 function stop(){
     clearInterval(timer);
-    console.log('stoped');
+    clearInterval(movingLine);
+    let resetLine = document.querySelector('.movingLine');
+    resetLine.style.left = `${playerPanelWidth + noteLength}px`;
 }
 
 function moveLine(){
+    
+    let lastMetronomeLine = document.querySelector('.lastmetronomeLine');
+    let LineDoMove = document.querySelector('.movingLine');
+    let noteLength = parseInt(document.querySelector('.note').offsetWidth);
     let lineWidth = document.querySelector('.line');
-    console.log(lineWidth.offsetWidth);
+    let firstMetronomeLine = document.querySelector('.metronomeLine');
+    let Speed = (parseInt(firstMetronomeLine.offsetLeft) - playerPanelWidth - noteLength)/50;
+
+
+    previousPos = parseInt(document.querySelector('.movingLine').offsetLeft);
+    LineDoMove.style.left = `${previousPos + Speed}px`;
+    let CurrentPosition = LineDoMove.style.left.slice(0,LineDoMove.style.left.length - 2);
+    let LeftSide = parseInt(lastMetronomeLine.style.left.slice(0,lastMetronomeLine.style.left.length - 2)) - 1;
+    let RightSide = parseInt(lastMetronomeLine.style.left.slice(0,lastMetronomeLine.style.left.length - 2)) + 1;
+    if(CurrentPosition > LeftSide && CurrentPosition < RightSide){
+        LineDoMove.style.left = `${playerPanelWidth + noteLength + 2}px`;
+    }
+
 }
 
 function metronomeSound(){
@@ -165,7 +232,6 @@ function MetronomeOnOff(btn){
 }
 
 function OnOff(btn){
-    console.log(btn.getAttribute('playable'))
     if(btn.getAttribute('playable') == 'true'){
         btn.setAttribute('playable',false);
         keyboardCanPlay = 'false';
@@ -200,72 +266,146 @@ function colorKeys(current){
 }
 
 function keyPlay(ev) {
+    let Xposition = document.querySelector('.movingLine').style.left;
+    let NoteDiv = document.createElement('div');
+    NoteDiv.style.width = '30px';
+    NoteDiv.style.height = '30px';
+    NoteDiv.style.backgroundColor = 'red';
+    NoteDiv.style.position = 'absolute';
+    NoteDiv.style.border = 'solid';
+    NoteDiv.style.borderColor = 'black';
+    NoteDiv.style.borderWidth = `0.5px`;
     if(keyboardCanPlay == 'false'){
         return;
     }
     if(event.code == 'KeyA'){
         keyPlayNote('C');
         let key = document.querySelector('#C');
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`C`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyW'){
         keyPlayNote('CS');
         let key = document.querySelector('#CS'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`CS`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyS'){
         keyPlayNote('D');
         let key = document.querySelector('#D'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`D`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyE'){
         keyPlayNote('DS');
         let key = document.querySelector('#DS'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`DS`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyD'){
         keyPlayNote('E');
         let key = document.querySelector('#E'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`E`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyF'){
         keyPlayNote('F');
         let key = document.querySelector('#F'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`F`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyT'){
         keyPlayNote('FS');
-        let key = document.querySelector('#FS'); 
+        let key = document.querySelector('#FS');
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`FS`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv); 
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyG'){
         keyPlayNote('G');
         let key = document.querySelector('#G'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`G`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyY'){
         keyPlayNote('GS');
         let key = document.querySelector('#GS'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`GS`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyH'){
         keyPlayNote('A');
         let key = document.querySelector('#A'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`A`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyU'){
         keyPlayNote('AS');
         let key = document.querySelector('#AS'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`AS`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyJ'){
         keyPlayNote('B');
         let key = document.querySelector('#B'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`B`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
     if(event.code == 'KeyK'){
         keyPlayNote('C5');
         let key = document.querySelector('#C5'); 
+        NoteDiv.style.left = `${Xposition}`;
+        NoteDiv.style.top = `${key.offsetTop}px`;
+        NoteDiv.classList.add(`C5`);
+        NoteDiv.classList.add(`${Xposition}`)
+        notes.appendChild(NoteDiv);
         colorKeys(key.firstChild);
     }
 }
